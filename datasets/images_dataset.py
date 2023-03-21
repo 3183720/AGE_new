35ae0aa7c61e2b15187e189466f3ef6a5e39fdef
3183720 <z3183720@ad.unsw.edu.au>
2023-03-21 10:46:02 +0000

    add class embedding for emotions

--- datasets/images_dataset.py
+++ datasets/images_dataset.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 from PIL import Image
@@ -7,17 +8,21 @@ from utils import data_utils
 class ImagesDataset(Dataset):
 
 	def __init__(self, source_root, target_root, opts, target_transform=None, source_transform=None):
-		self.source_paths = sorted(data_utils.make_dataset(source_root))
-		self.target_paths = sorted(data_utils.make_dataset(target_root))    
+		self.target_root = target_root     
+		self.source_paths = sorted(data_utils.make_dataset(source_root)) # folder of neutral expressions
+		self.target_paths = sorted(data_utils.make_dataset(target_root))  # folder of other expressions 
 		self.source_transform = source_transform
 		self.target_transform = target_transform
 		self.average_codes = torch.load(opts.class_embedding_path, map_location=torch.device("cpu"))
-		self.average_codes1 = torch.load(opts.secondary_class_embedding_path, map_location=torch.device("cpu"))
-		self.cate1_list=self.average_codes1.key()     
+		self.average_codes1 = torch.load(opts.secondary_class_embedding_path, map_location=torch.device("cpu"))   # class embedding for emotions
 		self.opts = opts
-	def find_matching_emotion_(self):
-     
-        
+	def find_matching_target_file(self,from_path):
+		cate = from_path.split('/')[-1].split('_')[0]      
+		files = [ idx for (idx, file) in enumerate(self.target_paths) if os.relpath(file, self.target_root).split('_')[0] == cate] # find images of the same identity in target folder
+		idx = np.random.choice( len(files) , 1) # randomly pick one emotion
+		to_path = files[idx]
+		cate1 = os.relpath(to_path, self.target_root).split('_')[1]     
+		return to_path, cate1      
 	def __len__(self):
 		return len(self.source_paths)
 
@@ -27,14 +32,11 @@ class ImagesDataset(Dataset):
 		cate1 = from_path.split('/')[-1].split('_')[1]      # category for emotion
 		from_im = Image.open(from_path)
 		from_im = from_im.convert('RGB') if self.opts.label_nc == 0 else from_im.convert('L')     
-		to_path = self.target_paths[index]
+		to_path, cate1= self.find_matching_target_file(from_path)
 		to_im = Image.open(to_path).convert('RGB')
 		if self.target_transform:
 			to_im = self.target_transform(to_im)
 
 		if self.source_transform:
 			from_im = self.source_transform(from_im)
-		else:
-			from_im = to_im
-
-		return from_im, to_im, self.average_codes[cate]
+		return from_im, to_im, self.average_codes[cate],self.average_codes1[cate1]
